7e0b5e010b3fe736a05e4182463f425057dbbffd
John MacFarlane <jgm@berkeley.edu>
2009-01-28 23:00:36 -0800

    Moved option parsing code into Gitit.Config.

---
+++ Gitit/Config.hs
@@ -0,0 +1,74 @@
+{-# LANGUAGE CPP #-}
+{-
+Copyright (C) 2009 John MacFarlane <jgm@berkeley.edu>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+-}
+
+{- Functions for parsing command line options and reading the config file. 
+-}
+
+module Gitit.Config ( getConfigFromOpts )
+where
+import Gitit.State (Config(..), defaultConfig)
+import System.Environment
+import System.Exit
+import System.IO (stderr, hPutStrLn)
+import System.Console.GetOpt
+import Control.Monad (liftM, foldM)
+
+data Opt
+    = Help
+    | ConfigFile FilePath
+    | Version
+    deriving (Eq)
+
+flags :: [OptDescr Opt]
+flags =
+   [ Option ['h'] [] (NoArg Help)
+        "Print this help message"
+   , Option ['v'] [] (NoArg Version)
+        "Print version information"
+   , Option ['f'] [] (ReqArg ConfigFile "FILE")
+        "Specify configuration file"
+   ]
+
+parseArgs :: [String] -> IO [Opt]
+parseArgs argv = do
+  progname <- getProgName
+  case getOpt Permute flags argv of
+    (opts,_,[])  -> return opts
+    (_,_,errs)   -> hPutStrLn stderr (concat errs ++ usageInfo (usageHeader progname) flags) >>
+                       exitWith (ExitFailure 1)
+
+usageHeader :: String -> String
+usageHeader progname = "Usage:  " ++ progname ++ " [opts...]"
+
+copyrightMessage :: String
+copyrightMessage = "\nCopyright (C) 2008 John MacFarlane\n" ++
+                   "This is free software; see the source for copying conditions.  There is no\n" ++
+                   "warranty, not even for merchantability or fitness for a particular purpose."
+
+handleFlag :: Config -> Opt -> IO Config
+handleFlag _ opt = do
+  progname <- getProgName
+  case opt of
+    Help         -> hPutStrLn stderr (usageInfo (usageHeader progname) flags) >> exitWith ExitSuccess
+    Version      -> hPutStrLn stderr (progname ++ " version " ++ _VERSION ++ copyrightMessage) >> exitWith ExitSuccess
+    ConfigFile f -> liftM read (readFile f)
+
+getConfigFromOpts :: IO Config
+getConfigFromOpts = getArgs >>= parseArgs >>= foldM handleFlag defaultConfig
+
