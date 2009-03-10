38352ad4ff4893e9f5890e687847c73259a89cb2
John MacFarlane <jgm@berkeley.edu>
2009-03-10 09:01:52 -0700

    Removed unneeded import in Gitit.Plugins.

--- Gitit/Plugins.hs
+++ Gitit/Plugins.hs
@@ -22,7 +22,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 module Gitit.Plugins ( loadPlugin )
 where
-import System.IO (hFlush, stdout)
 import System.FilePath
 import Gitit.State
 #ifdef _PLUGINS
