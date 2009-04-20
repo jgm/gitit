39f332ffdd897c6b8d5cfd7b7a4b5ec901a2bff8
John MacFarlane <jgm@johnsmac.gateway.2wire.net>
2009-04-19 20:54:01 -0700

    Change preview URL from /_preview to *___preview.
    Thanks to Henry Laxen for the idea.  This solves a residual
    problem running gitit with mod_proxy_html on an alternative
    docroot.  mod_proxy_html doesn't rewrite links in javascripts,
    so we changed preview.js to link to "___preview" instead of
    "/_preview", and we test for this suffix in the routing code.

--- js/preview.js
+++ js/preview.js
@@ -1,6 +1,6 @@
 function updatePreviewPane() {
     $("#previewpane").hide();
-    $("#previewpane").load("/_preview", { "raw" : $("#editedText").attr("value") });
+    $("#previewpane").load("___preview", { "raw" : $("#editedText").attr("value") });
     $("#previewpane").fadeIn(1000);
 };
 $(document).ready(function(){
