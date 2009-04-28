df4d6e8fcc08413cc87b66a9c4ea43ab7099d2af
John MacFarlane <jgm@johnsmac.gateway.2wire.net>
2009-04-27 21:55:29 -0700

    Reformatted code in Gitit/Layout.hs to make it more readable.

--- Gitit/Layout.hs
+++ Gitit/Layout.hs
@@ -22,7 +22,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 module Gitit.Layout ( PageLayout(..)
                     , Tab(..)
                     , defaultPageLayout
-                    , formattedPage 
+                    , formattedPage
                     )
 where
 import Data.FileStore
@@ -35,7 +35,7 @@ import Network.HTTP (urlEncodeVars)
 import Codec.Binary.UTF8.String (encodeString)
 import qualified Text.StringTemplate as T
 import Text.XHtml hiding ( (</>), dir, method, password, rev )
-import Data.Maybe (isNothing, isJust, mapMaybe, fromJust)
+import Data.Maybe (isNothing, isJust, fromJust)
 import Data.List (isSuffixOf)
 import Prelude hiding (catch)
 import Control.Exception (throwIO, catch)
@@ -50,7 +50,12 @@ data PageLayout = PageLayout
   , pgSelectedTab    :: Tab
   }
 
-data Tab = ViewTab | EditTab | HistoryTab | DiscussTab | DiffTab deriving (Eq, Show)
+data Tab = ViewTab
+         | EditTab
+         | HistoryTab
+         | DiscussTab
+         | DiffTab
+         deriving (Eq, Show)
 
 defaultPageLayout :: PageLayout
 defaultPageLayout = PageLayout
@@ -74,31 +79,18 @@ formattedPage layout page params htmlContents = do
             (EditTab,Just r)  -> return r
             _ -> return ""
   user <- getLoggedInUser params
+  let scripts  = ["jquery.min.js", "jquery-ui.packed.js"] ++ pgScripts layout
+  let scriptLink x = script ! [src ("/js/" ++ x),
+        thetype "text/javascript"] << noHtml
   let javascriptlinks = if null (pgScripts layout)
                            then ""
-                           else renderHtmlFragment $ concatHtml $ map
-                                  (\x -> script ! [src ("/js/" ++ x), thetype "text/javascript"] << noHtml)
-                                  (["jquery.min.js", "jquery-ui.packed.js"] ++ pgScripts layout)
+                           else renderHtmlFragment $ concatHtml $
+                                map scriptLink scripts
   let pageTitle = pgTitle layout `orIfNull` page
   let tabli tab = if tab == pgSelectedTab layout
                      then li ! [theclass "selected"]
                      else li
-  let origPage s = if ":discuss" `isSuffixOf` s then take (length s - 8) s else s
-  let linkForTab HistoryTab = Just $ tabli HistoryTab << anchor ! [href $ urlForPage page ++ "?history" ++ 
-                                                                    case rev of { Just r -> "&revision" ++ r; Nothing -> "" }] << "history"
-      linkForTab DiffTab    = Just $ tabli DiffTab << anchor ! [href ""] << "diff"
-      linkForTab ViewTab    = if isDiscussPage page
-                                 then Just $ tabli DiscussTab << anchor ! [href $ urlForPage $ origPage page] << "page"
-                                 else Just $ tabli ViewTab << anchor ! [href $ urlForPage page ++
-                                                                    case rev of { Just r -> "?revision=" ++ r; Nothing -> "" }] << "view"
-      linkForTab DiscussTab = Just $ tabli (if isDiscussPage page then ViewTab else DiscussTab) <<
-                                       anchor ! [href $ urlForPage page ++ if isDiscussPage page then "" else "?discuss"] << "discuss"
-      linkForTab EditTab    = Just $ tabli EditTab << anchor ! [href $ urlForPage page ++ "?edit" ++
-                                              (case rev of
-                                                    Just r   -> "&revision=" ++ r ++ "&" ++ urlEncodeVars [("logMsg", "Revert to " ++ r)]
-                                                    Nothing  -> "")] <<
-                                             if isNothing rev then "edit" else "revert"
-  let tabs = ulist ! [theclass "tabs"] << mapMaybe linkForTab (pgTabs layout)
+  let tabs = ulist ! [theclass "tabs"] << map (linkForTab tabli page rev) (pgTabs layout)
   let searchbox = gui ("/_search") ! [identifier "searchform"] <<
                          [ textfield "patterns"
                          , submit "search" "Search" ]
@@ -108,7 +100,8 @@ formattedPage layout page params htmlContents = do
   let messages = pMessages params
   let htmlMessages = if null messages
                         then noHtml
-                        else ulist ! [theclass "messages"] << map (li <<) messages
+                        else ulist ! [theclass "messages"] <<
+                          map (li <<) messages
   templ <- queryAppState template
   let filledTemp = T.render .
                    T.setAttribute "pagetitle" pageTitle .
@@ -117,14 +110,28 @@ formattedPage layout page params htmlContents = do
                    (case user of
                          Just u     -> T.setAttribute "user" u
                          Nothing    -> id) .
-                   (if isPage page then T.setAttribute "ispage" "true" else id) .
-                   (if pgShowPageTools layout then T.setAttribute "pagetools" "true" else id) .
-                   (if pPrintable params then T.setAttribute "printable" "true" else id) .
-                   (if isJust rev then T.setAttribute "nothead" "true" else id) .
-                   (if isJust rev then T.setAttribute "revision" (fromJust rev) else id) .
-                   (if null sha1 then id else T.setAttribute "sha1" sha1) .
-                   T.setAttribute "searchbox" (renderHtmlFragment (searchbox +++ gobox)) .
-                   T.setAttribute "exportbox" (renderHtmlFragment $  exportBox page params) .
+                   (if isPage page
+                       then T.setAttribute "ispage" "true"
+                       else id) .
+                   (if pgShowPageTools layout
+                       then T.setAttribute "pagetools" "true"
+                       else id) .
+                   (if pPrintable params
+                       then T.setAttribute "printable" "true"
+                       else id) .
+                   (if isJust rev
+                       then T.setAttribute "nothead" "true"
+                       else id) .
+                   (if isJust rev
+                       then T.setAttribute "revision" (fromJust rev)
+                       else id) .
+                   (if null sha1
+                       then id
+                       else T.setAttribute "sha1" sha1) .
+                   T.setAttribute "searchbox"
+                       (renderHtmlFragment (searchbox +++ gobox)) .
+                   T.setAttribute "exportbox"
+                       (renderHtmlFragment $  exportBox page params) .
                    T.setAttribute "tabs" (renderHtmlFragment tabs) .
                    T.setAttribute "messages" (renderHtmlFragment htmlMessages) .
                    T.setAttribute "content" (renderHtmlFragment htmlContents) $
@@ -134,10 +141,47 @@ formattedPage layout page params htmlContents = do
 exportBox :: String -> Params -> Html
 exportBox page params | (not (isSourceCode page)) =
   let rev = pRevision params
-  in  gui (urlForPage page) ! [identifier "exportbox"] << 
-        ([ textfield "revision" ! [thestyle "display: none;", value (fromJust rev)] | isJust rev ] ++
+  in  gui (urlForPage page) ! [identifier "exportbox"] <<
+        ([ textfield "revision" ! [thestyle "display: none;",
+             value (fromJust rev)] | isJust rev ] ++
          [ select ! [name "format"] <<
              map ((\f -> option ! [value f] << f) . fst) exportFormats
          , submit "export" "Export" ])
 exportBox _ _ = noHtml
+
+-- auxiliary functions:
+
+linkForTab :: (Tab -> Html -> Html) -> String -> Maybe String -> Tab -> Html
+linkForTab tabli page rev HistoryTab =
+  tabli HistoryTab << anchor ! [href $ urlForPage page ++ "?history" ++
+                                       case rev of
+                                            Just r -> "&revision" ++ r
+                                            Nothing -> "" ] << "history"
+linkForTab tabli _ _ DiffTab =
+  tabli DiffTab << anchor ! [href ""] << "diff"
+linkForTab tabli page rev ViewTab =
+  let origPage s = if ":discuss" `isSuffixOf` s
+                      then take (length s - 8) s
+                      else s
+  in if isDiscussPage page
+        then tabli DiscussTab << anchor !
+              [href $ urlForPage $ origPage page] << "page"
+        else tabli ViewTab << anchor !
+              [href $ urlForPage page ++
+                      case rev of
+                           Just r  -> "?revision=" ++ r
+                           Nothing -> ""] << "view"
+linkForTab tabli page _ DiscussTab =
+  tabli (if isDiscussPage page then ViewTab else DiscussTab) <<
+  anchor ! [href $ urlForPage page ++
+            if isDiscussPage page then "" else "?discuss"] << "discuss"
+linkForTab tabli page rev EditTab =
+  tabli EditTab << anchor !
+    [href $ urlForPage page ++ "?edit" ++
+            case rev of
+                  Just r   -> "&revision=" ++ r ++ "&" ++
+                               urlEncodeVars [("logMsg", "Revert to " ++ r)]
+                  Nothing  -> ""] << if isNothing rev
+                                         then "edit"
+                                         else "revert"
 
