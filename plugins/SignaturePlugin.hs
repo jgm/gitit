d4f1f87b141dc1b30706999dd33990aef3da2eec
John MacFarlane <jgm@johnsmac.gateway.2wire.net>
2009-05-02 13:12:35 -0700

    Added SignaturePlugin.hs to sample plugins (pre-commit example).

---
+++ plugins/SignaturePlugin.hs
@@ -0,0 +1,22 @@
+module SignaturePlugin (plugin) where
+
+-- This plugin replaces $SIG$ with the username and timestamp
+-- of the last edit, prior to saving the page in the repository.
+
+import Gitit.Interface
+import Data.Maybe (fromMaybe)
+import Data.DateTime
+import Control.Monad
+
+plugin :: Plugin
+plugin = PreCommitTransform replacedate
+
+replacedate :: String -> PluginM String
+replacedate [] = return ""
+replacedate ('$':'S':'I':'G':'$':xs) = do
+  datetime <- liftIO $ getCurrentTime
+  username <- liftM (fromMaybe "???") askUsername
+  let sig = "-- " ++ username ++ " (" ++ formatDateTime "%c" datetime ++ ")"
+  liftM (sig ++ ) $ replacedate xs
+replacedate (x:xs) = liftM (x : ) $ replacedate xs
+
