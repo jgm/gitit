f3c4e20e7e281fd4a2fc0e968a8b849777ee8e91
John MacFarlane <jgm@johnsmac.gateway.2wire.net>
2009-05-02 10:45:27 -0700

    Added ShowUserPlugin.hs.

---
+++ plugins/ShowUserPlugin.hs
@@ -0,0 +1,17 @@
+module ShowUserPlugin (plugin) where
+
+-- This plugin replaces $USER$ with the name of the currently logged in
+-- user, or the empty string if no one is logged in.
+
+import Gitit.Interface
+import Data.Maybe (fromMaybe)
+
+plugin :: Plugin
+plugin = mkPageTransformM showuser
+
+showuser :: Inline -> PluginM Inline
+showuser (Math InlineMath x) | x == "USER"  = do
+  doNotCache  -- tell gitit not to cache this page, as it has dynamic content
+  askUsername >>= return . Str . fromMaybe ""
+showuser x = return x
+
