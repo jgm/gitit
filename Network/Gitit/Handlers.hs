24695bf6ffd18034b29e5ef49b44c4fbbbd43fdb
John MacFarlane <jgm@berkeley.edu>
2009-06-15 00:19:52 -0700

    Moved wikiHandler function from gitit.hs to Network.Gitit.

--- Network/Gitit/Handlers.hs
+++ Network/Gitit/Handlers.hs
@@ -20,7 +20,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 {- Handlers for wiki functions.
 -}
 
-module Network.Gitit.Handlers ( handleAny
+module Network.Gitit.Handlers (
+                        handleAny
                       , debugHandler
                       , randomPage
                       , discussPage
