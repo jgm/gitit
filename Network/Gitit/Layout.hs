d7f616b791fce180d7cf65ff745db62672a4d9d4
John MacFarlane <jgm@berkeley.edu>
2009-07-10 21:36:30 -0700

    Mere reformatting.

--- Network/Gitit/Layout.hs
+++ Network/Gitit/Layout.hs
@@ -21,8 +21,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 -}
 
 module Network.Gitit.Layout ( defaultPageLayout
-                    , formattedPage
-                    )
+                            , formattedPage
+                            )
 where
 import Network.Gitit.Server
 import Network.Gitit.Framework
