bb79b2a97a8cb9ee1b12eec619108dbe9bececdd
John MacFarlane <jgm@johnsmac.gateway.2wire.net>
2009-09-28 20:15:48 -0700

    Fixed preview.js so that tex math works properly

--- data/static/js/preview.js
+++ data/static/js/preview.js
@@ -1,14 +1,7 @@
 function updatePreviewPane() {
     $("#previewpane").hide();
     var url = location.pathname.replace(/_edit\//,"_preview/");
-    $("#previewpane").load(url, { "raw" : $("#editedText").attr("value")
-                                , "format" : $("#format").attr("value")
-                                , "lhs": $("#lhs").attr("value")
-                                , "toc": $("#toc").attr("value")
-                                , "title": $("#title").attr("value")
-                                , "categories": $("#categories").attr("value")
-                                });
-    jQuery.getScript("/js/MathMLinHTML.js", function() {convert()});
+    $("#previewpane").load(url, { "raw" : $("#editedText").attr("value") }, function() {convert();} );
     $("#previewpane").fadeIn(1000);
 };
 $(document).ready(function(){
