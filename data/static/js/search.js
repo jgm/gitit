23b109d8aa1af16f4dd27871ff14cc3d011188b3
John MacFarlane <jgm@johnsmac.gateway.2wire.net>
2009-11-15 12:22:49 -0800

    Highlight search terms in search results.
    Partially resolves Issue #76.

--- data/static/js/search.js
+++ data/static/js/search.js
@@ -1,5 +1,20 @@
+jQuery.fn.highlightPattern = function (patt, className)
+{
+    // patt is a space separated list of strings - we want to highlight
+    // an occurrence of any of these strings as a separate word.
+    var regex = new RegExp('\\b(' + patt.replace(/ /, '|') + ')\\b', 'g');
+
+    return this.each(function ()
+    {
+        this.innerHTML = this.innerHTML.replace(regex,
+          '<span class=\'' + className + '\'>' + '$1' + '</span>');
+    });
+};
 function toggleMatches(obj) {
-  obj.next('.matches').slideToggle(300);
+  var pattern = $('#pattern').text();
+  var matches = obj.next('.matches')
+  matches.slideToggle(300);
+  matches.highlightPattern(pattern, 'highlighted');
   if (obj.html() == '[show matches]') {
       obj.html('[hide matches]');
     } else {
@@ -7,7 +22,7 @@ function toggleMatches(obj) {
     };
   }
 $(function() {
-  $('a.showmatch').attr("onClick", "toggleMatches($(this));");
+  $('a.showmatch').attr('onClick', 'toggleMatches($(this));');
   $('pre.matches').hide();
   $('a.showmatch').show();
   });
