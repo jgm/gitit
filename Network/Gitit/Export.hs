f1ea12b4116c57aad1d67cfeed481bc42622870c
John MacFarlane <jgm@johnsmac.gateway.2wire.net>
2010-01-02 13:12:54 -0800

    Modified Export to work with pandoc 1.4.

--- Network/Gitit/Export.hs
+++ Network/Gitit/Export.hs
@@ -22,6 +22,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 module Network.Gitit.Export ( exportFormats )
 where
 import Text.Pandoc
+import Text.Pandoc.Writers.S5 (s5HeaderIncludes)
 import Text.Pandoc.ODT (saveOpenDocumentAsODT)
 import Network.Gitit.Server
 import Network.Gitit.Util (withTempDir)
@@ -31,6 +32,7 @@ import Control.Monad.Trans (liftIO)
 import Text.XHtml (noHtml)
 import qualified Data.ByteString.Lazy as B
 import System.FilePath ((<.>), (</>))
+import Control.Exception (throwIO)
 
 defaultRespOptions :: WriterOptions
 defaultRespOptions = defaultWriterOptions { writerStandalone = True
@@ -46,52 +48,69 @@ respond mimetype ext fn page = ok . setContentType mimetype .
   (if null ext then id else setFilename (page ++ "." ++ ext)) .
   toResponse . fn
 
+respondX :: String -> String -> String -> (WriterOptions -> Pandoc -> String)
+         -> WriterOptions -> String -> Pandoc -> Handler
+respondX templ mimetype ext fn opts page doc = do
+  template' <- liftIO $ getDefaultTemplate templ
+  template <- case template' of
+                  Right t  -> return t
+                  Left e   -> liftIO $ throwIO e
+  respond mimetype ext (fn opts{writerTemplate = template}) page doc 
+
 respondLaTeX :: String -> Pandoc -> Handler
-respondLaTeX = respond "application/x-latex" "tex" $
-  writeLaTeX (defaultRespOptions {writerHeader = defaultLaTeXHeader})
+respondLaTeX = respondX "latex" "application/x-latex" "tex"
+  writeLaTeX defaultRespOptions
 
 respondConTeXt :: String -> Pandoc -> Handler
-respondConTeXt = respond "application/x-context" "tex" $
-  writeConTeXt (defaultRespOptions {writerHeader = defaultConTeXtHeader})
+respondConTeXt = respondX "context" "application/x-context" "tex"
+  writeConTeXt defaultRespOptions
 
 respondRTF :: String -> Pandoc -> Handler
-respondRTF = respond "application/rtf" "rtf" $
-  writeRTF (defaultRespOptions {writerHeader = defaultRTFHeader})
+respondRTF = respondX "rtf" "application/rtf" "rtf"
+  writeRTF defaultRespOptions
 
 respondRST :: String -> Pandoc -> Handler
-respondRST = respond "text/plain; charset=utf-8" "" $
-  writeRST (defaultRespOptions {writerHeader = "", writerReferenceLinks = True})
+respondRST = respondX "rst" "text/plain; charset=utf-8" ""
+  writeRST defaultRespOptions{writerReferenceLinks = True}
 
 respondMan :: String -> Pandoc -> Handler
-respondMan = respond "text/plain; charset=utf-8" "" $
-  writeMan (defaultRespOptions {writerHeader = ""})
+respondMan = respondX "man" "text/plain; charset=utf-8" ""
+  writeMan defaultRespOptions
 
 respondS5 :: String -> Pandoc -> Handler
-respondS5 _ = ok . toResponse .
-  writeS5 (defaultRespOptions {writerHeader = defaultS5Header,
-            writerS5 = True, writerIncremental = True})
+respondS5 pg doc = do
+  inc <- liftIO $ s5HeaderIncludes
+  respondX "s5" "text/html; charset=utf-8" ""
+    writeS5String
+    defaultRespOptions{writerS5 = True, writerIncremental = True,
+                       writerVariables = [("header-includes",inc)]}
+    pg doc
 
 respondTexinfo :: String -> Pandoc -> Handler
-respondTexinfo = respond "application/x-texinfo" "texi" $
-  writeTexinfo (defaultRespOptions {writerHeader = ""})
+respondTexinfo = respondX "texinfo" "application/x-texinfo" "texi"
+  writeTexinfo defaultRespOptions
 
 respondDocbook :: String -> Pandoc -> Handler
-respondDocbook = respond "application/docbook+xml" "xml" $
-  writeDocbook (defaultRespOptions {writerHeader = defaultDocbookHeader})
+respondDocbook = respondX "docbook" "application/docbook+xml" "xml"
+  writeDocbook defaultRespOptions
 
 respondMediaWiki :: String -> Pandoc -> Handler
-respondMediaWiki = respond "text/plain; charset=utf-8" "" $
-  writeMediaWiki (defaultRespOptions {writerHeader = ""})
+respondMediaWiki = respondX "mediawiki" "text/plain; charset=utf-8" ""
+  writeMediaWiki defaultRespOptions
 
 respondODT :: String -> Pandoc -> Handler
 respondODT page doc = do
+  template' <- liftIO $ getDefaultTemplate "odt"
+  template <-  case template' of
+                  Right t  -> return t
+                  Left e   -> liftIO $ throwIO e
   let openDoc = writeOpenDocument
-                (defaultRespOptions {writerHeader = defaultOpenDocumentHeader})
+                defaultRespOptions{writerTemplate = template}
                 doc
   conf <- getConfig
   contents <- liftIO $ withTempDir "gitit-temp-odt" $ \tempdir -> do
                 let tempfile = tempdir </> page <.> "odt"
-                saveOpenDocumentAsODT tempfile (repositoryPath conf) openDoc
+                saveOpenDocumentAsODT tempfile (repositoryPath conf) Nothing openDoc
                 B.readFile tempfile
   ok $ setContentType "application/vnd.oasis.opendocument.text" $
        setFilename (page ++ ".odt") $ (toResponse noHtml) {rsBody = contents}
