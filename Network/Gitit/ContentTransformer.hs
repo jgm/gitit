jgm/gitit

commit 2128afb070b7ea17f96e57d726e00727be4d4e1a

Fixed URL encoding for pages.
Note: we don't want to use + for space; that breaks the
Ctrl-R cache expiration.

Network/Gitit/ContentTransformer.hs
@@ -85,11 +85,9 @@ import Text.Pandoc.Shared (ObfuscationMethod(..))
 import Text.XHtml hiding ( (</>), dir, method, password, rev )
 import Text.Highlighting.Kate
 import Data.Maybe (isNothing)
-import Codec.Binary.UTF8.String (encodeString)
 import System.FilePath
 import Control.Monad.State
 import Control.Exception (throwIO, catch)
-import Network.URI (isUnescapedInURI, escapeURIString)
 import qualified Data.ByteString as S (concat) 
 import qualified Data.ByteString.Lazy as L (toChunks, fromChunks)
 
@@ -507,7 +505,7 @@ convertWikiLinks x = x
 
 -- | Derives a URL from a list of Pandoc Inline elements.
 inlinesToURL :: [Inline] -> String
-inlinesToURL = escapeURIString isUnescapedInURI  . encodeString . inlinesToString
+inlinesToURL = urlForPage . inlinesToString
 
 -- | Convert a list of inlines into a string.
 inlinesToString :: [Inline] -> String

Network/Gitit/Framework.hs
@@ -60,7 +60,7 @@ import Network.Gitit.Server
 import Network.Gitit.State
 import Network.Gitit.Types
 import Data.FileStore
-import Data.Char (toLower, isAscii)
+import Data.Char (toLower)
 import Control.Monad (mzero, liftM, MonadPlus)
 import qualified Data.Map as M
 import Data.ByteString.UTF8 (toString)
@@ -71,6 +71,7 @@ import System.FilePath ((<.>), takeExtension)
 import Text.Highlighting.Kate
 import Text.ParserCombinators.Parsec
 import Network.URL (decString, encString)
+import Network.URI (isUnescapedInURI)
 import Happstack.Crypto.Base64 (decode)
 import Network.HTTP (urlEncodeVars)
 
@@ -272,8 +273,7 @@ isSourceCode path' =
 -- the wiki base.
 urlForPage :: String -> String
 urlForPage page = "/" ++
-  encString True (\c -> isAscii c && (c `notElem` "?&")) page
--- / and @ are left unescaped so that browsers recognize relative URLs and talk pages correctly
+  encString False isUnescapedInURI page
 
 -- | Returns the filestore path of the file containing the page's source.
 pathForPage :: String -> FilePath

commit 1ae17e1bfb754c757afaad54174c76a395f8a576

Fix wikilinks to they don't get a leading slash.
This reverts a bug introduced by 2128afb070b7,
which added leading slashes to wikilinks, breaking
them for people using gitit as a library on a path
other than /.

Thanks to MightyByte for reporting.

Network/Gitit/ContentTransformer.hs
@@ -90,6 +90,8 @@ import Control.Monad.State
 import Control.Exception (throwIO, catch)
 import qualified Data.ByteString as S (concat) 
 import qualified Data.ByteString.Lazy as L (toChunks, fromChunks)
+import Network.URL (encString)
+import Network.URI (isUnescapedInURI)
 
 --
 -- ContentTransformer runners
@@ -505,7 +507,7 @@ convertWikiLinks x = x
 
 -- | Derives a URL from a list of Pandoc Inline elements.
 inlinesToURL :: [Inline] -> String
-inlinesToURL = urlForPage . inlinesToString
+inlinesToURL = encString False isUnescapedInURI . inlinesToString
 
 -- | Convert a list of inlines into a string.
 inlinesToString :: [Inline] -> String