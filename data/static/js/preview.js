9c5ee85fa56360b792ada962e2c63054e80f7c07
Sean Seefried <sean.seefried@gmail.com>
2010-11-10 12:10:46 +1100

    jQuery.load wasn't working on Safari. Use jQuery.post instead.
    Also, the convert function was not always in scope. (It is only in
    scope when MathML is enabled?) We check if it is defined before
    calling it.

--- data/static/js/preview.js
+++ data/static/js/preview.js
@@ -1,8 +1,15 @@
 function updatePreviewPane() {
     $("#previewpane").hide();
     var url = location.pathname.replace(/_edit\//,"_preview/");
-    $("#previewpane").load(url, { "raw" : $("#editedText").attr("value") }, function() {convert();} );
-    $("#previewpane").fadeIn(1000);
+    $.post(
+      url,
+      {"raw" : $("#editedText").attr("value")},
+      function(data) {
+        $('#previewpane').html(data);
+        if (typeof(convert) == 'function') { convert(); }
+      },
+      "html");
+    $('#previewpane').fadeIn(1000);
 };
 $(document).ready(function(){
     $("#previewButton").show();
