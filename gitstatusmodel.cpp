fe48c5dcdf09296538f23df7498b008782abbfdf
hef <hef@pbrfrat.com>
2010-11-22 20:50:56 -0600

    cleaning out debug code

--- gitstatusmodel.cpp
+++ gitstatusmodel.cpp
@@ -15,18 +15,14 @@ int GitStatusModel::rowCount(const QModelIndex & /* parent */) const
         return 0;
     else
     {
-        git_index_read(gitIndex);
-        qDebug() << "git_index_endtrycount(gitIndex)" << git_index_entrycount(gitIndex);
         return git_index_entrycount(gitIndex);
     }
 }
 
 QVariant GitStatusModel::data(const QModelIndex &index, int role) const
 {
-    qDebug() << QDateTime::currentDateTime() << "GitStatModel::data(" << index << "," << role << ") called";
     if (gitIndex==NULL)
     {
-        qDebug() << "gitIndex is not set.";
         return QVariant();
     }
     if (!index.isValid())
@@ -35,17 +31,15 @@ QVariant GitStatusModel::data(const QModelIndex &index, int role) const
         return QVariant();
     if (role == Qt::DisplayRole)
     {
-    qDebug() << "index.row()" << index.row() << git_index_get(gitIndex, index.row())->path;
         return QString(git_index_get(gitIndex, index.row())->path);
     }
     return QVariant();
 }
 void GitStatusModel::update(git_repository* gitRepo)
 {
-    qDebug() << "GitStatusModel::update(" << gitRepo << ")";
     gitIndex = git_repository_index(gitRepo);
+    git_index_read(gitIndex);
     //TODO check for off by 1
     //TODO check old size versus new size.
     emit dataChanged( createIndex(0,0), createIndex( git_index_entrycount(gitIndex),0 ) );
-    qDebug() << "gitIndex set";
 }
