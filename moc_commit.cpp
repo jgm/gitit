57d67ee549325bc11b772c501c4fb91fd3c8e8d3
Bry <bdenne2@uic.edu>
2010-11-22 18:11:42 -0600

    changed the look of the gui

---
+++ moc_commit.cpp
@@ -0,0 +1,69 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'commit.h'
+**
+** Created: Mon Nov 22 18:01:20 2010
+**      by: The Qt Meta Object Compiler version 62 (Qt 4.6.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "commit.h"
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'commit.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 62
+#error "This file was generated using the moc from 4.6.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+static const uint qt_meta_data_Commit[] = {
+
+ // content:
+       4,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       0,    0, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+       0        // eod
+};
+
+static const char qt_meta_stringdata_Commit[] = {
+    "Commit\0"
+};
+
+const QMetaObject Commit::staticMetaObject = {
+    { &QObject::staticMetaObject, qt_meta_stringdata_Commit,
+      qt_meta_data_Commit, 0 }
+};
+
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &Commit::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
+
+const QMetaObject *Commit::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
+}
+
+void *Commit::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_Commit))
+        return static_cast<void*>(const_cast< Commit*>(this));
+    return QObject::qt_metacast(_clname);
+}
+
+int Commit::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    return _id;
+}
+QT_END_MOC_NAMESPACE
---
+++ moc_configure.cpp
@@ -0,0 +1,69 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'configure.h'
+**
+** Created: Mon Nov 22 18:01:21 2010
+**      by: The Qt Meta Object Compiler version 62 (Qt 4.6.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "configure.h"
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'configure.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 62
+#error "This file was generated using the moc from 4.6.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+static const uint qt_meta_data_Configure[] = {
+
+ // content:
+       4,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       0,    0, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+       0        // eod
+};
+
+static const char qt_meta_stringdata_Configure[] = {
+    "Configure\0"
+};
+
+const QMetaObject Configure::staticMetaObject = {
+    { &QDialog::staticMetaObject, qt_meta_stringdata_Configure,
+      qt_meta_data_Configure, 0 }
+};
+
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &Configure::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
+
+const QMetaObject *Configure::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
+}
+
+void *Configure::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_Configure))
+        return static_cast<void*>(const_cast< Configure*>(this));
+    return QDialog::qt_metacast(_clname);
+}
+
+int Configure::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QDialog::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    return _id;
+}
+QT_END_MOC_NAMESPACE
