8d07f4dd0406ec2549a06fb9b2ecfcfec5264d78
hef <hef@pbrfrat.com>
2010-12-02 20:33:38 -0600

    adding appicaiton name

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setApplicationName("gitit");
+    a.setOrganizationName("BMH Software");
     MainWindow w;
     w.show();
 
