afc8355020c43d55aebaf00a1e653524efc1f263
Sean Seefried <sean.seefried@gmail.com>
2011-01-10 11:07:29 +1100

    MathML and jsMath math now shows in preview pane

--- data/static/js/preview.js
+++ data/static/js/preview.js
@@ -6,10 +6,15 @@ function updatePreviewPane() {
       {"raw" : $("#editedText").attr("value")},
       function(data) {
         $('#previewpane').html(data);
+        // Process any mathematics if we're using MathML
         if (typeof(convert) == 'function') { convert(); }
+        // Process any mathematics if we're using jsMath
+        if (typeof(jsMath) == 'object')    { jsMath.ProcessBeforeShowing(); }
       },
       "html");
+
     $('#previewpane').fadeIn(1000);
+
 };
 $(document).ready(function(){
     $("#previewButton").show();
