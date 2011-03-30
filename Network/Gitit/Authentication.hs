4239488cf17ea7b9feb4f98d79193e4654c1d38c
John MacFarlane <jgm@berkeley.edu>
2011-03-30 09:42:20 -0700

    Removed RPX athentication option.
    This brought in too many external dependencies, including
    some not satisfiable with the latest Haskell Platform.
    It would be great if someone could write a lightweight RPX
    module with few dependencies.

--- Network/Gitit/Authentication.hs
+++ Network/Gitit/Authentication.hs
@@ -23,8 +23,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 module Network.Gitit.Authentication ( loginUserForm
                                     , formAuthHandlers
-                                    , httpAuthHandlers
-                                    , rpxAuthHandlers) where
+                                    , httpAuthHandlers ) where
 
 import Network.Gitit.State
 import Network.Gitit.Types
@@ -42,16 +41,13 @@ import System.Exit
 import System.Log.Logger (logM, Priority(..))
 import Data.Char (isAlphaNum, isAlpha, isAscii)
 import Text.Pandoc.Shared (substitute)
-import Data.Maybe (isJust, fromJust, isNothing, fromMaybe)
+import Data.Maybe (isJust, fromJust )
 import Network.URL (encString, exportURL, add_param, importURL)
 import Network.BSD (getHostName)
 import qualified Text.StringTemplate as T
-import Network.HTTP (urlEncodeVars, urlDecode, urlEncode)
+import Network.HTTP (urlEncodeVars)
 import Codec.Binary.UTF8.String (encodeString)
 import Data.ByteString.UTF8 (toString)
-import Control.Monad.Reader (runReaderT, ask)
-import qualified Web.Authenticate.Rpxnow as R
-import qualified Network.URI as U
 
 data ValidationType = Register
                     | ResetPassword
@@ -431,61 +427,6 @@ httpAuthHandlers :: [Handler]
 httpAuthHandlers =
   [ dir "_logout" $ logoutUserHTTP
   , dir "_login"  $ withData loginUserHTTP
-  , dir "_user" currentUser ]
-
--- Login using RPX (see RPX development docs at https://rpxnow.com/docs)
-loginRPXUser :: RPars  -- ^ The parameters passed by the RPX callback call (after authentication has taken place
-             -> Handler
-loginRPXUser params = do
-  cfg <- getConfig
-  refer <- liftM U.parseURI getReferer
-  liftIO $ logM "gitit.loginRPXUser" DEBUG $ "Referer:" ++ show refer ++ " params: " ++ show params
-  let mtoken = rToken params
-  if isNothing mtoken
-     then do -- Initial call from the user
-       if isNothing refer
-          then see $ fromMaybe "/" $ rDestination params
-          else do -- Redirect user to RPX login
-            let ref = fromJust refer
-            let url = ref {U.uriPath="/_login",U.uriQuery="?destination=" ++ (fromMaybe (U.uriPath ref) $ rDestination params)}
-            if null (rpxDomain cfg)
-               then error "rpx-domain is not set."
-               else do
-                  let rpx = "https://" ++ rpxDomain cfg ++ ".rpxnow.com/openid/v2/signin?token_url=" ++ urlEncode (show url)
-                  see rpx
-     else do -- We got an answer from RPX, this might also return an exception.
-       uid :: R.Identifier <- liftIO $ R.authenticate (rpxKey cfg) $ fromJust mtoken
-       liftIO $ logM "gitit.loginRPXUser" DEBUG $ "uid:" ++ show uid
-       -- We need to get an unique identifier for the user
-       -- The 'identifier' is always present but can be rather cryptic
-       -- The 'verifiedEmail' is also unique and is a more readable choice
-       -- so we use it if present.
-       let userId = R.identifier uid
-       let email  = prop "verifiedEmail" uid
-       key <- newSession (SessionData userId)
-       addCookie (sessionTimeout cfg) (mkCookie "sid" (show key))
-       user <- liftIO $ mkUser (fromMaybe userId email) (fromMaybe "" email) "none"
-       addUser userId user
-       see $ fromJust $ rDestination params
-      where
-        prop pname info = lookup pname $ R.extraData info
-        see url = seeOther (encUrl url) $ toResponse noHtml
-
--- The parameters passed by the RPX callback call.
-data RPars = RPars {rToken::Maybe String,rDestination::Maybe String} deriving Show
-
-instance FromData RPars where
-     fromData = do
-         let look' = liftM urlDecode . look
-         env <- ask
-         let vtoken = runReaderT (look "token") env
-         let vDestination = runReaderT (look' "destination") env
-         return RPars {rToken=vtoken,rDestination=vDestination}
-
-rpxAuthHandlers :: [Handler]
-rpxAuthHandlers =
-  [ dir "_logout" $ methodSP GET $ withData logoutUser
-  , dir "_login"  $ withData loginRPXUser
   , dir "_user" currentUser ]
 
 -- | Returns username of logged in user or null string if nobody logged in.
--- Network/Gitit/Config.hs
+++ Network/Gitit/Config.hs
@@ -27,7 +27,7 @@ where
 import Network.Gitit.Types
 import Network.Gitit.Server (mimeTypes)
 import Network.Gitit.Framework
-import Network.Gitit.Authentication (formAuthHandlers, rpxAuthHandlers, httpAuthHandlers)
+import Network.Gitit.Authentication (formAuthHandlers, httpAuthHandlers)
 import Network.Gitit.Util (parsePageType, readFileUTF8)
 import System.Log.Logger (logM, Priority(..))
 import qualified Data.Map as M
@@ -89,8 +89,6 @@ extractConfig cp = do
       cfUseRecaptcha <- get cp "DEFAULT" "use-recaptcha"
       cfRecaptchaPublicKey <- get cp "DEFAULT" "recaptcha-public-key"
       cfRecaptchaPrivateKey <- get cp "DEFAULT" "recaptcha-private-key"
-      cfRPXDomain <- get cp "DEFAULT" "rpx-domain"
-      cfRPXKey <- get cp "DEFAULT" "rpx-key"
       cfCompressResponses <- get cp "DEFAULT" "compress-responses"
       cfUseCache <- get cp "DEFAULT" "use-cache"
       cfCacheDir <- get cp "DEFAULT" "cache-dir"
@@ -118,8 +116,6 @@ extractConfig cp = do
                         "darcs"     -> Darcs
                         "mercurial" -> Mercurial
                         x           -> error $ "Unknown repository type: " ++ x
-      when (authMethod == "rpx" && cfRPXDomain == "") $
-         liftIO $ logM "gitit" WARNING $ "rpx-domain is not set"
 
       return $! Config{
           repositoryPath       = cfRepositoryPath
@@ -135,7 +131,6 @@ extractConfig cp = do
         , withUser             = case authMethod of
                                       "form"     -> withUserFromSession
                                       "http"     -> withUserFromHTTPAuth
-                                      "rpx"      -> withUserFromSession
                                       _          -> id
         , requireAuthentication = case map toLower cfRequireAuthentication of
                                        "none"    -> Never
@@ -146,7 +141,6 @@ extractConfig cp = do
         , authHandler          = case authMethod of
                                       "form"     -> msum formAuthHandlers
                                       "http"     -> msum httpAuthHandlers
-                                      "rpx"      -> msum rpxAuthHandlers
                                       _          -> mzero
         , userFile             = cfUserFile
         , sessionTimeout       = readNumber "session-timeout" cfSessionTimeout * 60  -- convert minutes -> seconds
@@ -175,8 +169,6 @@ extractConfig cp = do
         , useRecaptcha         = cfUseRecaptcha
         , recaptchaPublicKey   = cfRecaptchaPublicKey
         , recaptchaPrivateKey  = cfRecaptchaPrivateKey
-        , rpxDomain            = cfRPXDomain
-        , rpxKey               = cfRPXKey
         , compressResponses    = cfCompressResponses
         , useCache             = cfUseCache
         , cacheDir             = cfCacheDir
