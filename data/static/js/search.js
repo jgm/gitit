f2b8f9e87bb1cdac1281aed0174064f55d7a2554
Caleb McDaniel <caleb.mcdaniel@rice.edu>
2014-06-19 03:38:26 -0500

    global replace spaces, make regex case insensitive

--- data/static/js/search.js
+++ data/static/js/search.js
@@ -2,7 +2,7 @@ jQuery.fn.highlightPattern = function (patt, className)
 {
     // patt is a space separated list of strings - we want to highlight
     // an occurrence of any of these strings as a separate word.
-    var regex = new RegExp('\\b(' + patt.replace(/ /, '|') + ')\\b', 'g');
+    var regex = new RegExp('\\b(' + patt.replace(/ /g, '|') + ')\\b', 'gi');
 
     return this.each(function ()
     {
