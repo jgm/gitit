c3ad386cf6acc71c8719f1289cc5dd79081fae09
Caleb McDaniel <caleb.mcdaniel@rice.edu>
2014-07-07 14:22:08 -0500

    escape regex metacharacters in the patt

--- data/static/js/search.js
+++ data/static/js/search.js
@@ -1,8 +1,12 @@
 jQuery.fn.highlightPattern = function (patt, className)
 {
+
+    // escape regex metacharacters that may be in the patt
+    var epatt = patt.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+
     // patt is a space separated list of strings - we want to highlight
     // an occurrence of any of these strings as a separate word.
-    var regex = new RegExp('\\b(' + patt.replace(/ /g, '|') + ')\\b', 'gi');
+    var regex = new RegExp('\\b(' + epatt.replace(/ /g, '|') + ')\\b', 'gi');
 
     return this.each(function ()
     {
