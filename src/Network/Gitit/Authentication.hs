5b29d6e93feea95ccf8b370844303641690726e2
Rob Zinkov <rob@zinkov.com>
2021-03-01 00:43:39 +0000

    Display correct html

--- src/Network/Gitit/Authentication.hs
+++ src/Network/Gitit/Authentication.hs
@@ -350,7 +350,7 @@ loginForm dest = do
       , textfield "destination" ! [thestyle "display: none;", value dest]
       , submit "login" "Login" ! [intAttr "tabindex" 3]
       ] +++
-    if not (disableRegistration cfg)
+    if disableRegistration cfg
        then noHtml
        else p << [ stringToHtml "If you do not have an account, "
                  , anchor ! [href $ base' ++ "/_register?" ++
